8135f677e41e42bf845047ab782202e44a7c62b3
RCarlos <robertoherrera113@gmail.com>
2024-07-10 07:27:58 -0600

    Prototype

--- boot.py
+++ boot.py
@@ -5,12 +5,27 @@
 #webrepl.start()
 from servo import Servo
 import time
+from machine import Pin, I2C
+import ssd1306
 
 # Create network connection
-
+i2c = I2C(sda=Pin(4), scl=Pin(5))
+display = ssd1306.SSD1306_I2C(128, 64, i2c)
+display.poweron()
+display.fill(0)
+display.fill_rect(0, 0, 32, 32, 1)
+display.fill_rect(2, 2, 28, 28, 0)
+display.vline(9, 8, 22, 1)
+display.vline(16, 2, 22, 1)
+display.vline(23, 8, 22, 1)
+display.fill_rect(26, 24, 2, 4, 1)
+display.text('MicroPython', 40, 0, 1)
+display.text('SSD1306', 40, 12, 1)
+display.text('OLED 128x64', 40, 24, 1)
+display.show()
 
 # Test servos
-motor=Servo(pin=17) # A changer selon la broche utilisée
+motor=Servo(pin=13) # A changer selon la broche utilisée
 motor.move(135)
 
 while motor.current_angle > 90:
@@ -34,6 +49,18 @@ print(motor.current_angle)
 
 
 # Wait for a successful connection
-
+import network
+sta_if = network.WLAN(network.STA_IF)
+if not sta_if.isconnected():
+    print('connecting to network...')
+    sta_if.active(True)
+    sta_if.connect('esp', '32323232')
+    while not sta_if.isconnected():
+        pass
+print('network config:', sta_if.ifconfig()[0])
 
 # Set OLED message
+display.fill(0)
+display.text('Connected', 0, 0)
+display.text(sta_if.ifconfig()[0], 0, 10)
+display.show()
--- main.py
+++ main.py
@@ -1,54 +1,134 @@
 # import libraries
+import time
+import network
+from machine import Pin, PWM, I2C
+from servo import Servo
+import ssd1306
+
+# Functions
+def toggle_pin(pin):
+    if pin.value() == 0:
+        pin.value(1)
+    else:
+        pin.value(0)
 
 # Pin assignment
-i2c_sda = 4
-i2c_scl = 5
+pin_i2c_sda     = 04
+pin_i2c_scl     = 05
 
-i_estopBtn = 6
-o_estopBtn = 7
+pin_i_estopBtn = 06
+pin_o_estopBtn = 07
+pin_i_greenBtn = 09
+pin_o_greenBtn = 10
+pin_i_IR    = 11
+pin_o_IR    = 12
+pin_o_sevo1 = 13
+pin_o_sevo2 = 14
+pin_o_buzz  = 15
 
-i_greenBtn = 9
-o_greenBtn = 10
+i2c         = I2C(sda=Pin(pin_i2c_sda), scl=Pin(pin_i2c_scl))
+i_estopBtn  = Pin(pin_i_estopBtn,   Pin.IN, Pin.PULL_DOWN)
+o_estopBtn  = Pin(pin_o_estopBtn,   Pin.OUT)
+i_greenBtn  = Pin(pin_i_greenBtn,   Pin.IN, Pin.PULL_DOWN)
+o_greenBtn  = Pin(pin_o_greenBtn,   Pin.OUT)
+i_IR        = Pin(pin_i_IR,         Pin.IN, Pin.PULL_DOWN)
+o_IR        = Pin(pin_o_IR,         Pin.OUT)
+o_sevo1     = Servo(pin=pin_o_sevo1)
+o_sevo2     = Servo(pin=pin_o_sevo2)
+o_buzz      = Pin(pin_o_buzz,       Pin.OUT)
+oled        = ssd1306.SSD1306_I2C(128, 64, i2c)
 
-i_IR = 11
-o_IR = 12
+# Startup
+## Variable setting
+oled.poweron()
+ticket_not_valid_blink_counter = 0
+ticket_valid = 0 # 0: not read, 1: valid, 2: not valid
+## Wifi config
+while sta_if.isconnected == False:
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    sta_if.connect('esp', '32323232')
+    time.sleep(2)
 
-o_sevo1 = 13
-o_sevo2 = 14
-
-o_buzz = 15
-
+# Main func --------------------
 # Estop behavior
+tmr_estop_bh_start = time.ticks_ms()
+while i_estopBtn.value() == 1:
+    oled.fill(0)
+    oled.text('E-Stop enabled', 0, 0)
+    oled.show()
+    # blink red LED
+    tmr_estop_bh_loop = time.ticks_ms()
+    if time.ticks_diff(tmr_estop_bh_loop, tmr_estop_bh_start) > 300:
+        toggle_pin(o_estopBtn)
+        tmr_estop_bh_start = time.ticks_ms()
+    # Set status screen
+o_estopBtn.off()
 
+# Clear screen
+oled.fill(0)
+oled.show()
 
 # Read NFC tag
 
 
 # Query DB
+ticket_valid = int(input('0/1/2: '))
 
 
 # If ticket not valid
-    # Blink red LED
-
+tmr_estop_bh_start = time.ticks_ms()
+while ticket_valid == 2:
     # Show message on screen
+    oled.fill(0)
+    oled.text('Ticket not valid', 0, 0)
+    oled.show()
 
+    # Blink red LED
+    tmr_estop_bh_loop = time.ticks_ms()
+    if time.ticks_diff(tmr_estop_bh_loop, tmr_estop_bh_start) > 300:
+        if ticket_not_valid_blink_counter < 4:
+            toggle_pin(o_estopBtn)
+            ticket_not_valid_blink_counter += 1
+            tmr_estop_bh_start = time.ticks_ms()
+        else:
+            o_estopBtn.off()
+            ticket_valid = 0
+            oled.fill(0)
+            oled.show()
+
+    
 # If valid
+while ticket_valid == 1:
     # Show user name on OLED
+    #oled.fill(0)
+    #oled.show()
 
-    # Blink green LED
+    # Turn on green LED
+    o_greenBtn.value(1)
 
     # Open barrier
+    o_sevo1.move(90)
+    o_sevo2.move(90)
 
     # Register in DB
 
     # Delay
+    time.sleep(2.5)
 
     # Wait for IR sensor to be 0
 
     # Delay
+    time.sleep(0.5)
 
     # Close barrier
-
+    o_sevo1.move(180)
+    o_sevo2.move(0)
     # Clear OLED
+    oled.fill(0)
+    oled.show()
 
+    # Resume normal operation
+    ticket_valid = 0
+    o_greenBtn.value(0)
 
